25ab534f97061899de1c1843224ee8f87f194f36
tj2904 <tj2904+github@gmail.com>
2022-03-28 11:45:38 +0100

    adds collapsing behaviour to navbar & bootstrap js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,13 +4,16 @@ import { NavLink } from "react-router-dom";
 function Navigation() {
   return (
     <div className="navigation">
-      <nav className="navbar navbar-expand-md navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
           <NavLink className="navbar-brand" to="/">
           <i class="bi bi-camera-reels-fill d-inline-block align-text-middle"></i>
           &nbsp;&nbsp;My Media Trainer
           </NavLink>
-          <div>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+          <div className="collapse navbar-collapse" id="navbar">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                 <NavLink className="nav-link" to="/">
