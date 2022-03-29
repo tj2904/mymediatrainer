b7723c347efcdece69cdb5d0b40cfb0f969f25ee
Tim <tj2904@users.noreply.github.com>
2022-03-29 14:06:17 +0100

    Update Home.jsx
    Fixes typo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="home ">
+    <div className="home">
       <div className="container pt-4">
         <h1 className="font-weight-light">My Media Trainer</h1>
         <p>This is a temporary holding Main/Home page.</p>
         <p>
-          Using the links in the Navigation bar to see other pages and content.
+          Use the links in the Navigation bar to see other pages and content.
         </p>
       </div>
     </div>
