0c669bbe48aa4498619867aa14c1f761810287da
Tim <tj2904@users.noreply.github.com>
2022-03-29 17:01:21 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,12 @@ import React from "react";
 function Home() {
   return (
     <div className="home">
-MT-35-produce-mock-up-that-follows-the-approved-wireframes
       <div className="container pt-4">
         <h1 className="font-weight-light">My Media Trainer</h1>
         <p>This is a temporary holding Main/Home page.</p>
         <p>
           Use the links in the Navigation bar to see other pages and content.
         </p>
-
       </div>
     </div>
   );
