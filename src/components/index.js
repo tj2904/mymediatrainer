9552e867631e943f4eeff450d76f72b0aff620ea
tj2904 <tj2904+github@gmail.com>
2022-04-12 20:06:59 +0100

    resolving merge conflicts

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,4 @@ export { default as Home } from "./Home";
 export { default as About } from "./About";
 export { default as Contact } from "./Contact";
 export { default as VideoSingleTextQuestion } from "./VideoSingleTextQuestion";
-export {default as VideoSelectResponse} from "./VideoSelectResponse";
 export {default as VideoUpload} from "./VideoUpload"
